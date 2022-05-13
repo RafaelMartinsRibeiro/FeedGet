e1da8ec06f4434ce1e0d8e90615d4c05e23cdde4
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-13 15:35:02 -0300

    Added CloseButton component

--- NlwReturn/src/components/WidgetForm.tsx
+++ NlwReturn/src/components/WidgetForm.tsx
@@ -1,7 +1,26 @@
+import { CloseButton } from "./CloseButton";
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-      Hello World
+      <header>
+        <span className="text-xl leading-6">Deixe seu feedback</span>
+        <CloseButton />
+      </header>
+
+      <p>
+        
+      </p>
+
+      <footer className="text-xs text-neutral-400">
+        Feito com ♥ por{" "}
+        <a
+          className="underline underline-offset-2"
+          href="https://github.com/RafaelMartinsRibeiro"
+        >
+          Rafael
+        </a>
+      </footer>
     </div>
   );
 }
