47f02f9e86fb367fcc83435da491bc443d651072
Rafael Martins <rafaelmartins1964@hotmail.com>
2022-05-13 16:43:59 -0300

    Added feedbackTypes

--- NlwReturn/src/components/WidgetForm.tsx
+++ NlwReturn/src/components/WidgetForm.tsx
@@ -1,5 +1,33 @@
 import { CloseButton } from "./CloseButton";
 
+import bugImageUrl from "../assets/bug.svg";
+import ideaImageUrl from "../assets/idea.svg";
+import thoughtImageUrl from "../assets/thought.svg";
+
+const feedbackType = {
+  BUG: {
+    title: "Problema",
+    image: {
+      source: bugImageUrl,
+      alt: "Imagem de um inseto",
+    },
+  },
+  IDEA: {
+    title: "Ideia",
+    image: {
+      source: ideaImageUrl,
+      alt: "Imagem de uma lâmpada",
+    },
+  },
+  OTHER: {
+    title: "Outro",
+    image: {
+      source: thoughtImageUrl,
+      alt: "Imagem de uma nuvem de pensamento",
+    },
+  },
+};
+
 export function WidgetForm() {
   return (
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,14 +36,24 @@ export function WidgetForm() {
         <CloseButton />
       </header>
 
-      <p>
-        
-      </p>
+      <div className="flex py-8 gap-2 w-full">
+        {Object.entries(feedbackType).map(([key, value]) => {
+          return (
+            <button
+              key={key}
+              className="w-24 h-28 bg-zinc-800 rounded-lg py-5 flex flex-col items-center flex-1 gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+            >
+              <img src={value.image.source} alt={value.image.alt}></img>
+              <span>{value.title}</span>
+            </button>
+          );
+        })}
+      </div>
 
       <footer className="text-xs text-neutral-400">
         Feito com ♥ por{" "}
         <a
-          className="underline underline-offset-2"
+          className="underline underline-offset-2 hover:text-brand-500"
           href="https://github.com/RafaelMartinsRibeiro"
         >
           Rafael
